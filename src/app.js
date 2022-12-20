1af5b31305e9d85dc4e05a6cccf0ec6bb341a45a
Atiqur Rahman <atiqur.03@gmail.com>
2022-12-21 01:40:17 +0530

    Removing posts

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { ui } from "./ui"
 
 document.addEventListener("DOMContentLoaded", getPosts)
 document.querySelector(".post-submit").addEventListener("click", submitPost)
+document.querySelector("#posts").addEventListener("click", deletePost)
 
 function getPosts() {
   http
@@ -28,4 +29,20 @@ function submitPost() {
       getPosts()
     })
     .catch((err) => console.log(err))
+}
+
+function deletePost(e) {
+  e.preventDefault()
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id
+    if (confirm("Are you sure?")) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert("Post Removed", "alert alert-success")
+          getPosts()
+        })
+        .catch((err) => console.log(err))
+    }
+  }
 }
